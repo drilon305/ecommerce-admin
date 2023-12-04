616dd02e8d87b1bac604df4a6bb56ed053665a3c
Drilon <navycrack1@gmail.com>
2023-12-04 23:51:55 +0100

    displayed products in page

--- app/api/products/page.js
+++ app/api/products/page.js
@@ -1,9 +1,13 @@
-import { Product } from '../../../models/Product'
-import { mongooseConnect } from '../../lib/mongoose'
+import { Product } from '../Product'
+import { mongooseConnect } from '../../app/lib/mongoose'
 
 export default async function handle(req, res) {
     const {method} = req;
     await mongooseConnect()
+
+    if(method === 'GET') {
+        res.json(await Product.find())
+    }
 
     if(method === 'POST') {
         const {title, description, price} = req.body;
@@ -13,3 +17,4 @@ export default async function handle(req, res) {
         res.json(productDoc)
     }
 }
+
